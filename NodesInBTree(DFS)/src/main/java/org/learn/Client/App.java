36f9cac70a922fd7c560a2db00e2f9ecaf0c5ec0
makeinjava <makeinjava@gmail.com>
2016-07-25 13:57:08 +0530

    Updated binary tree structure

--- NodesInBTree(DFS)/src/main/java/org/learn/Client/App.java
+++ NodesInBTree(DFS)/src/main/java/org/learn/Client/App.java
@@ -6,15 +6,18 @@ import org.learn.Question.NodesInBTree;
 public class App {
 	public static void main(String[] args) {
 		// root level 0
-		Node A = Node.createNode(100);
+		Node A = Node.createNode(50);
 		// Level 1
-		Node B = Node.createNode(50);
-		Node C = Node.createNode(150);
+		Node B = Node.createNode(30);
+		Node C = Node.createNode(90);
 		// Level 2
-		Node D = Node.createNode(25);
-		Node E = Node.createNode(80);
-		Node F = Node.createNode(125);
-		Node G = Node.createNode(160);
+		Node D = Node.createNode(10);
+		Node E = Node.createNode(40);
+		Node F = Node.createNode(60);
+		Node G = Node.createNode(95);
+		// Level 2
+		Node H = Node.createNode(55);
+		Node I = Node.createNode(65);
 
 		// connect Level 0 and 1
 		A.left = B;
@@ -24,8 +27,12 @@ public class App {
 		B.right = E;
 		C.left = F;
 		C.right = G;
+
+		// connect level 2 and level 3
+		F.left = H;
+		F.right = I;
+
 		int nodes = NodesInBTree.nodesInBTree(A);
 		System.out.println("Nodes of Binary Tree : " + nodes);
-
 	}
 }
